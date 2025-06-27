207704b087d95385aaa46ba04731094e8af5a8b2
Xue Min <495888983@qq.com>
2025-06-27 11:43:11 +0800

    修改中继服务器域名和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.jhjhjhjhj.top"];
+pub const RS_PUB_KEY: &str = "YUAg2VW8B1eyoFoiDs0W6WnHGvSj7rtSan8VnT30LjI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
